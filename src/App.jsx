f555ced1051dc77f0742e9cdba59a23b87385bdb
Abril Zacarias <abrilzacarais6@gmail.com>
2023-09-15 09:46:56 -0300

    :p

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,114 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//constante para los turnoa
+const TURNS = {
+  X: 'x',
+  O: 'o',
+}
 
+
+
+const Square = ({children,  isSelected, updateBoard, index}) => {
+  //let className = 'square';
+  //if (isSelected) {
+  //  className += ' selected';
+  //} TERNARIO asi
+  const className = `square ${isSelected ? 'is-selected': ''}`
+  const handleClick = () => {
+    updateBoard(index)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onClick={handleClick} className={className}>
+        {children}
+    </div>
+)}
+
+//key es un identificador unico
+//la lista de elementos se renderiza con .map, devuleve un array
+//entonces, renderiza cada elemento dentro de section game y se tiene que identificar cada elemento con una key
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+function App() {
+  //tablero
+  //Se necesita un estado para guardar donde el usuario hace click
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  //otro estado para saber el turno del usuario
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) //null = no hay ganador, false = empate
+
+  const checkWinner = (boardToCheck) => {
+    //se revisa todas las combinaciones ganadores para ver si X u O gana, se chequea siempre
+    for (combo in WINNER_COMBOS){
+    const [a, b, c] = combo
+    //se verifica si la X o la O esta en todas esas posiciones
+    if (boardToCheck[a] && boardToCheck[a] == boardToCheck[b] && boardToCheck[a] == boardToCheck[c])
+    {return boardToCheck[a] }
+    //si no hay ganador
+    return null
+  }
+
+  }
+  const updateBoard = (index) => {
+    //para que no se actualice la posicion si ya tiene algo, o si hay un ganador
+    if (board[index] || winner) return 
+    
+    //se crea una copia y no se hace un board[index] = turn (o sea, modificar)
+    //porque no se tiene que modificar NUNCA las props y el estado, se debe crear
+    // un nuevo array, en esta caso se utiliza el array de abajo (una copia)
+    const newBoard = [...board]
+    //spread y rest operator
+    //el utilizado es una COPIA SUPERFICIAL, si se necesita hacer una
+    //de forma profunda se utiliza structuredClone(board)
+    //a los estados y props se los trata como INMUTABLES porque
+    //si se lo modifica, pueden ocurrir problemas de renderizado xq se
+    //modifica el estado directamente en lugar de utilizar setBoard
+    //por lo tanto los datos de renderizado siempre tiene que ser nuevos
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn == TURNS.X ? TURNS.O: TURNS.X
+    setTurn(newTurn)
+    //revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+    //la actualizacion de los estados en React, son asincronos
+    //no bloquea la ejecucion del codigo que sigue
+      setWinner(newWinner)}
+  }
+   
+  return (
+    <main className='board'>
+      <section className='game'> {
+        board.map((_, index) => {
+          return (
+            <Square
+            key={index}
+            index={index}
+            updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          )}
+        )}
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn == TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn == TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
   )
+    
 }
 
 export default App
